8965ecaf163fd123a5bc29067b8829b07d8f79b7
sjw3957 <sjw3957@gmail.com>
2022-01-09 11:12:20 +0900

    통계 서비스 init

---
+++ stats-service/src/test/java/com/poscoict/mes/statsservice/StatsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poscoict.mes.statsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
