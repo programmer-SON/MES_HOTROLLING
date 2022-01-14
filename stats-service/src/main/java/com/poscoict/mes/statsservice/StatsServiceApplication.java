6657374324ebe3dcc0d3a38db7337df3c68666ae
huny <43372897+Shon-BH@users.noreply.github.com>
2022-01-14 16:08:59 +0900

    통계 유레카/게이트웨이 설정

--- stats-service/src/main/java/com/poscoict/mes/statsservice/StatsServiceApplication.java
+++ stats-service/src/main/java/com/poscoict/mes/statsservice/StatsServiceApplication.java
@@ -2,8 +2,10 @@ package com.poscoict.mes.statsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StatsServiceApplication {
 
     public static void main(String[] args) {
