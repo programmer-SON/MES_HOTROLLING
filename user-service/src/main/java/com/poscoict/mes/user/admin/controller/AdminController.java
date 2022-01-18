506cb01be6e10d4c8f71df3aacdb8d3559b9647e
Programmer-SON <sjw3957@gmail.com>
2022-01-18 15:51:44 +0900

    Admin Controller uri 수정

--- user-service/src/main/java/com/poscoict/mes/user/admin/controller/AdminController.java
+++ user-service/src/main/java/com/poscoict/mes/user/admin/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.poscoict.mes.user.admin.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.poscoict.mes.user.admin.jpa.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -12,40 +13,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.poscoict.mes.user.admin.jpa.AdminProductEntity;
-import com.poscoict.mes.user.admin.jpa.AdminProductRepository;
-import com.poscoict.mes.user.admin.jpa.CompanyEntity;
-import com.poscoict.mes.user.admin.jpa.CompanyRepository;
 import com.poscoict.mes.user.admin.vo.ResponseAdminProduct;
 import com.poscoict.mes.user.admin.vo.ResponseCompany;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/admin")
 public class AdminController {
 	
 	AdminProductRepository adminProductRepository;
 	CompanyRepository companyRepository;
+	ProductRepository productRepository;
+
 
 	
 	@Autowired
 	public AdminController(AdminProductRepository adminProductRepository,
-			CompanyRepository companyRepository) {
+							CompanyRepository companyRepository,
+						   ProductRepository productRepository) {
 		this.adminProductRepository = adminProductRepository;
 		this.companyRepository = companyRepository;
+		this.productRepository = productRepository;
 	}
 
 	
-	@GetMapping("/product_log")
+	@GetMapping("/products")
 	public ResponseEntity<List<ResponseAdminProduct>> getAdminProduct(){		
-		Iterable<AdminProductEntity> adminProductList = adminProductRepository.findAll();
+		Iterable<ProductEntity> adminProductList = productRepository.findAll();
 		List<ResponseAdminProduct> result = new ArrayList<>();
 		
 		adminProductList.forEach(v -> {
 			result.add(new ModelMapper().map(v, ResponseAdminProduct.class));
 		});
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Type", "application/json");
 		
 		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
@@ -62,5 +60,7 @@ public class AdminController {
 
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
+
+
 	
 }
